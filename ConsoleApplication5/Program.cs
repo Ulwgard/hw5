e63ae7b12a4996d33cfa3d40aef1ed48c31a2645
Vladislav Vladyslavych <flatronx19932@gmail.com>
2016-09-14 16:01:48 +0300

    first co

---
+++ ConsoleApplication5/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication5
+
+
+    {
+        class Program
+        {
+            static void Main(string[] args)
+            {
+                int a = Convert.ToInt32(Console.ReadLine());
+                if (step(a)) Console.WriteLine("yes");
+                else Console.WriteLine("No");
+                Console.ReadKey();
+            }
+            public static bool step(int a)
+            {
+                if (a == 2) return true;
+                else if (a % 2 == 0) return step(a / 2);
+                else return false;
+            }
+        }
+   }
+
+
---
+++ hw5/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace hw5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = Convert.ToInt32(Console.ReadLine());
+            if (step(a)) Console.WriteLine("yes");
+            else Console.WriteLine("No");
+            Console.ReadKey();
+        }
+        public static bool step(int a)
+        { if (a % 2 == 0) return true;
+            else return false;
+        }
+    }
+}
+    
