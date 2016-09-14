ed94aa5d1928b437a33071a20d3d903557de4f55
Vladislav Vladyslavych <flatronx19932@gmail.com>
2016-09-14 18:40:30 +0300

    first com

---
+++ ConsoleApplication2/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            bool A = true, B = false;
+            Console.WriteLine("{0} || {1} = {2}", A, B, A || B);
+            Console.WriteLine("!(!{0} && !{1}) = {2}", A, B, !(!A & !B));
+            Console.ReadKey();
+
+        }
+    }
+}
